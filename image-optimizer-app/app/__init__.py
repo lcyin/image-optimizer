467c09ecd85b04b32fcdd51b25ba1ca5e51ea44d
lcyin <laichunyinkevin@gmailcom>
2025-03-22 18:25:49 +0800

    Add html and Fix ruote

--- image-optimizer-app/app/__init__.py
+++ image-optimizer-app/app/__init__.py
@@ -8,6 +8,8 @@ def configure_app(app):
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
     configure_app(app)
+    with app.app_context():
+        from . import routes
 
     try:
         app.config.from_pyfile('config.py', silent=True)
--- image-optimizer-app/app/routes.py
+++ image-optimizer-app/app/routes.py
@@ -1,9 +1,17 @@
-from flask import render_templates
-from app import create_app
+from flask import render_template
+from flask import current_app as app
 
-app = create_app()
+# app = create_app()
 
 @app.route('/')
 def home():
+    print('we got here')
     return render_template('base.html')
 
+# add route to see all registered routes
+@app.route('/routes')
+def routes():
+    routes = []
+    for rule in app.url_map.iter_rules():
+        routes.append(f"{rule.endpoint} - {rule.rule}")
+    return  "<br>".join(routes)
